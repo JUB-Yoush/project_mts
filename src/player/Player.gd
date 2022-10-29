a45c1b4ebcebe882d2f3b73cdf5f40cde2d73646
JUB_Yoush <yoush1337@gmail.com>
2022-10-29 00:22:50 -0400

    starting skating, it's not going too hot rn

--- src/player/Player.gd
+++ src/player/Player.gd
@@ -1,39 +1,24 @@
 extends KinematicBody
 
-var max_speed: = 10
-var move_speed: = 10
-var gravity: = -80.0
-var jump_impulse: = 25.0
 onready var camera:CameraRig = $CameraRig
-
+onready var dirRay:RayCast = $DirRay
 var velocity: = Vector3.ZERO
 
-static func get_input_direction() -> Vector3:
-	#map a 2d input vector to 3d space
+var speed:= 3;
+var steer_angle: = 3
+
+
+static func get_input_vector() -> Vector3:
 	var iv =  Input.get_vector("move_left", "move_right", "move_front", "move_back")
 	return Vector3(iv.x,0.0,iv.y)
 
+
 func _physics_process(delta: float) -> void:
-	var input_direction: = get_input_direction()
-	var forwards:Vector3 = camera.global_transform.basis.z * input_direction.z
-	var right:Vector3 = camera.global_transform.basis.x * input_direction.x
-	var move_direction := forwards + right
-	if move_direction.length() > 1.0:
-		move_direction = move_direction.normalized()
-	move_direction.y = 0.0
-
-	if move_direction:
-		look_at(global_transform.origin + move_direction,Vector3.UP)
-	
-	velocity = calculate_velocity(velocity,move_direction,delta)
-	velocity = move_and_slide(velocity,Vector3.UP)
-	print(velocity)
-	
-
-func calculate_velocity(velocity_current:Vector3,move_direction:Vector3,delta:float) -> Vector3:
-	var velocity_new:= velocity_current
-	velocity_new = move_direction * move_speed
-	if velocity_new.length() > max_speed:
-		velocity_new = velocity_new.normalized() * max_speed
-	velocity_new.y = velocity_current.y + gravity * delta
-	return velocity_new
+	#y effects speed
+	#x effects rotation
+	var input_vector: = get_input_vector()
+	var forwards:Vector3 = camera.global_transform.basis.z * input_vector.z
+	var right:Vector3 = camera.global_transform.basis.x * input_vector.x
+	var rot_change: = steer_angle * input_vector.x
+	rotation_degrees.y += -rot_change
+	velocity = move_and_slide(Vector3.FORWARD * speed)

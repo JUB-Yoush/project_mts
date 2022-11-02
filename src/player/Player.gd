efc19d51a351d38e9efaf04ba1455f33d0553579
JUB_Yoush <yoush1337@gmail.com>
2022-11-01 23:42:04 -0400

    some JSRF notes

--- src/player/Player.gd
+++ src/player/Player.gd
@@ -14,18 +14,22 @@ static func get_input_vector() -> Vector3:
 
 
 func _physics_process(delta: float) -> void:
-	#y effects speed
-	#x effects rotation
+	#you always start by moving forward.
+	# inputting past 90deg to the left or right  when stopped will turn you around to face backwards and then move in that direction
+	# the allowed inputs are greater when you're moving, maybe like a 30deg radius around the opposite direction of where you're current velocity is
+	#you start by moving forward then begin to rotate towards the input direction
+	#the speed at which you rotate is based on your current speed, going faster means you turn slower
+	
+	
 	var input_vector: = get_input_vector()
 	var forwards:Vector3 = camera.global_transform.basis.z * input_vector.z
 	var right:Vector3 = camera.global_transform.basis.x * input_vector.x
 	var relative_input_vector: = forwards + right
+	relative_input_vector.normalized()
 	var rot_change: = steer_angle * relative_input_vector.x
 	rotation_degrees.y += -rot_change
 	velocity.x = -cos(rotation.y)
 	velocity.z = sin(rotation.y)
 	velocity.normalized()
-	print(velocity)
-	print(rotation_degrees.y)
-	#prints(cos(rotation_degrees.y),sin(rotation_degrees.y))
-	velocity = move_and_slide(velocity * speed)
+	prints(input_vector,relative_input_vector)
+	velocity = move_and_slide(velocity* (speed * -input_vector.z))

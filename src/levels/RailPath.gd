03e55007acbef8a8f0b430e7137660557965b452
JUB_Yoush <yoush1337@gmail.com>
2022-11-12 10:21:51 -0500

    rails don't poop

--- src/levels/RailPath.gd
+++ src/levels/RailPath.gd
@@ -4,18 +4,23 @@ onready var player = get_parent().get_node("Player")
 onready var railFollow = $RailFollow
 onready var startPos: = $StartPosition
 onready var endPos: = $EndPosiiton
+onready var followTarget := $FollowTarget
 var length_vector:Vector3
 var player_riding:= false
 
 func _ready() -> void:
 	length_vector = endPos.global_translation - startPos.global_translation
 	print(length_vector)
+	
 
 func _physics_process(delta: float) -> void:
-	#print(curve.get_closest_offset(player.global_translation))
+	followTarget.global_transform.origin = player.global_transform.origin
 	if not player_riding: 
-		railFollow.offset = curve.get_closest_offset(player.global_translation)
+		railFollow.set_offset(curve.get_closest_offset(followTarget.get_translation()))
+		#railFollow.offset = curve.get_closest_offset(transform.origin - player.transform.origin)
+		
 	print(railFollow.offset)
+	#print(player.rotation - rotation)
 	#railFollow.offset = curve.get_closest_offset(player.global_translation)
 	
 
